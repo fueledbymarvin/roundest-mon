96b585e984d89ecf564733249852ba380e237316
Marvin Qian <fueledbymarvin@gmail.com>
2022-08-19 00:06:05 -0400

    Add random pokemon

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,16 +1,17 @@
+import { getOptionsForVote } from "@/utils/pokemon";
 import { trpc } from "@/utils/trpc";
 import type { NextPage } from "next";
 
 const Home: NextPage = () => {
-  const { data, isLoading } = trpc.useQuery(["hello", { text: "Hi" }]);
+  const [first, second] = getOptionsForVote();
   return (
     <div className="h-screen w-screen flex flex-col justify-center items-center">
       <div className="text-2xl text-center">Which Pokemon is rounder?</div>
       <div className="p-2" />
       <div className="border rounded p-8 flex justify-between items-center max-w-2xl">
-        <div className="w-16 h-16 bg-purple-400"></div>
+        <div className="w-16 h-16 bg-purple-400">{first}</div>
         <div className="p-8">Vs</div>
-        <div className="w-16 h-16 bg-pink-400"></div>
+        <div className="w-16 h-16 bg-pink-400">{second}</div>
       </div>
     </div>
   );
